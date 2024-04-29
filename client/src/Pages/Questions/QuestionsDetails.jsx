10fa5b26b5df498c3bfdae5e8a6743995a02bfda
Car-ram-el <nityachi@gmail.com>
2024-04-29 08:10:14 +0530

    update

--- client/src/Pages/Questions/QuestionsDetails.jsx
+++ client/src/Pages/Questions/QuestionsDetails.jsx
@@ -20,7 +20,7 @@ const QuestionsDetails = () => {
   const dispatch = useDispatch();
   const User = useSelector((state) => state.currentUserReducer);
   const location = useLocation();
-  const url = "https://main--clone-stack-over-flow.netlify.app/";
+  const url = "https://main--clone-stack-over-flow.netlify.app";
 
   const handlePostAns = (e, answerLength) => {
     e.preventDefault();
